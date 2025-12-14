bd4e23fdcebfd308193497946f33741ce7604680
Ragavi-cit <ragavipasuran28@gmail.com>
2025-12-14 09:49:27 +0530

    Add files via upload
    Initial commit - Calculator App

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Form from"./Form";  
+import'./Form.css';
+// import './App.css';
+import Todo from"./Todo";
+function App() {
+ return (
+    <div>
+       <Form/> 
+      {/* <Todo/> */}
+    </div>
+  );
+}
+
+export default App;
