bda7ec183cb1f98cff6d42c1198ff0dc782cd1ad
Ragavi-cit <ragavipasuran28@gmail.com>
2025-12-14 10:04:04 +0530

    Remove wrong App.css content

--- App.jsx
+++ App.jsx
@@ -1,14 +1,1 @@
-import Form from"./Form";  
-import'./Form.css';
-// import './App.css';
-import Todo from"./Todo";
-function App() {
- return (
-    <div>
-       <Form/> 
-      {/* <Todo/> */}
-    </div>
-  );
-}
 
-export default App;
